612ed2bc3c9dd2436794e140e13b0a25428b960a
Andrew S <andrews54757@gmail.com>
2021-11-14 19:25:50 -0600

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,7 @@ const path = require('path')
 
 const secret = JSON.parse(fs.readFileSync(path.join(__dirname, 'secret.json'), 'utf8'))
 const bot = new Bot(secret)
+
+process.on('unhandledRejection', error => {
+  console.error('Unhandled promise rejection:', error);
+});
