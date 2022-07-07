b13c69b8fb5ecb3ff3c15940fb3f3c5a6a9e0084
Andrew S <andrews54757@gmail.com>
2022-07-06 21:04:00 -0400

    Update

--- src/enum/SampleSupport.js
+++ src/enum/SampleSupport.js
@@ -1,9 +1,9 @@
 const SampleSupport = {
-  UNKNOWN: 'unknown',
-  SUPPORTED: 'supported',
-  ANON_BOMBED: 'anon-bombed',
-  OVERSATURATED: 'oversaturated',
-  UNSUPPORTED: 'unsupported'
+  UNKNOWN: 'Unknown',
+  SUPPORTED: 'Supported',
+  ANON_BOMBED: 'Anon-bombed',
+  OVERSATURATED: 'Oversaturated',
+  UNSUPPORTED: 'Unsupported'
 }
 
 module.exports = SampleSupport
