ea4fc7ecdd981868017662890225ab2d4726134a
Andrew S <andrews54757@gmail.com>
2022-07-06 21:11:41 -0400

    Update

--- src/enum/Constants.js
+++ src/enum/Constants.js
@@ -1,6 +1,6 @@
 const Constants = {
   ANAONYMOUS_UUID: '00000000-0000-0000-0000-000000000000',
-  RICK: 'https://youtu.be/dQw4w9WgXcQ'
+  RICK: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 }
 
 module.exports = Constants
